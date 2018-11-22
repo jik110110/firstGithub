33509b4779f8f0ef2d037b5c4b914e7de0d2263f
jik110110 <1003919376@qq.com>
2018-11-22 19:48:01 +0800

    再试一下111和222的合并

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,12 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-<<<<<<< .mine
-        System.out.println("2222222222222222222222");
-
-=======
-             System.out.println("11111111111111");
-
->>>>>>> .theirs
+        System.out.println("2222222222222222222222")
+        System.out.println("11111111111111");
     }
 }
