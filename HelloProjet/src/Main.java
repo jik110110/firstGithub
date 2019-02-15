bbacc7a31b6f33d4982f157807ed6228a423837d
jik110110 <1003919376@qq.com>
2019-02-16 00:59:17 +0800

    aaaaaaaaaaaaaaaaaaaaa

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,12 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("2222222222222222222222")
+        System.out.println("2222222222222222222222");
         System.out.println("11111111111111");
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
